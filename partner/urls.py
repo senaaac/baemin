e41079341e76fdda9acaf4054417f1d79733585f
“senaaac” <listen.and.listen@gmail.com>
2017-01-15 15:07:36 +0900

    week04_homework

---
+++ partner/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import url, include
+from .views import (
+    index,
+    signup,
+    login,
+    logout,
+    edit_info,
+    menu, menu_add, menu_detail, menu_edit,menu_delete
+)
+
+urlpatterns = [
+    url(r'^$', index, name="index" ),
+    url(r'^signup/$', signup, name="signup" ),
+    url(r'^login/$', login, name="login" ),
+    url(r'^logout/$', logout, name="logout" ),
+    url(r'^edit_info/$', edit_info, name="edit_info" ),
+    url(r'^menu/$', menu, name="menu" ),
+    url(r'^menu/add/$', menu_add, name="menu_add" ),
+    url(r'^menu/(?P<menu_id>\d+)/$', menu_detail, name="menu_detail" ),
+    url(r'^menu/(?P<menu_id>\d+)/edit/$', menu_edit, name="menu_edit" ),
+    url(r'^menu/(?P<menu_id>\d+)/delete/$', menu_delete, name="menu_delete" ),
+]
--- templates/urls.py
+++ templates/urls.py
@@ -17,14 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('client.urls')),
     url(r'^partner/', include('partner.urls')),
     url(r'^admin/', admin.site.urls),
 ]
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
